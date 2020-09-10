779eb65f0fd3bd1830047dd89d32ed31e1384a43
grace <graceokereke22@gmail.com>
2020-09-11 01:40:16 +1400

    changes

--- index.js
+++ index.js
@@ -1,11 +1,104 @@
 function display(num) {
-    var figure = num.innerText;
-    if(num == "AC"){
+    var arr = document.getElementById("display").innerText;
+    if (num == "AC") {
         document.getElementById("display").innerText = 0;
     }
-    else if(num == "="){
-        document.getElementById("display").innerText = eval(document.getElementById("display").innerText)
-    }
+    else if ((num == "=") && (arr.includes("+")) && (arr.includes("."))) {
+        let array = arr.split("+");
+        let arrays = [];
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            arrays.push(parseFloat(array[i]));
+        }
+        for (let i = 0; i < arrays.length; i++) {
+            total += arrays[i];
+            document.getElementById("display").innerHTML = total;
+        }
+    } 
+    else if (num == "=" && arr.includes("+")) {
+
+        let array = arr.split("+");
+        let arrays = [];
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            arrays.push(parseInt(array[i]));
+        }
+        for (let i = 0; i < arrays.length; i++) {
+            total += arrays[i];
+            document.getElementById("display").innerHTML = total;
+        }
+    } 
+    else if ((num == "=") && (arr.includes("-")) && (arr.includes("."))) {
+        let array = arr.split("-");
+        let arrays = [];
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            arrays.push(parseFloat(array[i]));
+        }
+        for (let i = 0; i < arrays.length; i++) {
+            total = arrays[0] - arrays[i];
+            document.getElementById("display").innerHTML = total;
+        }
+    } 
+    else if (num == "=" && arr.includes("-")) {
+        let array = arr.split("-");
+        let arrays = [];
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            arrays.push(parseInt(array[i]));
+        }
+        for (let i = 0; i < arrays.length; i++) {
+            total = arrays[0] - arrays[i];
+            document.getElementById("display").innerHTML = total;
+        }
+    } 
+    else if ((num == "=") && (arr.includes("/")) && (arr.includes("."))) {
+        let array = arr.split("/");
+        let arrays = [];
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            arrays.push(parseFloat(array[i]));
+        }
+        for (let i = 0; i < arrays.length; i++) {
+            total = arrays[0] / arrays[i];
+            document.getElementById("display").innerHTML = total.toFixed(4);
+        }
+    } 
+    else if (num == "=" && arr.includes("/")) {
+        let array = arr.split("/");
+        let arrays = [];
+        for (let i = 0; i < array.length; i++) {
+            arrays.push(parseInt(array[i]));
+        }
+        for (let i = 0; i < arrays.length; i++) {
+           let total = arrays[0] / arrays[i];
+
+            document.getElementById("display").innerHTML = total.toFixed(4);
+        }
+    } 
+    else if ((num == "=") && (arr.includes("*")) && (arr.includes("."))) {
+        let array = arr.split("*");
+        let arrays = [];
+        let total = 0;
+        for (let i = 0; i < array.length; i++) {
+            arrays.push(parseFloat(array[i]));
+        }
+        for (let i = 0; i < arrays.length; i++) {
+            total = arrays[0] * arrays[i];
+            document.getElementById("display").innerHTML = total.toFixed(4);
+        }
+    } 
+    else if (num == "=" && arr.includes("*")) {
+        let array = arr.split("*");
+        let arrays = [];
+        for (let i = 0; i < array.length; i++) {
+            arrays.push(parseInt(array[i]));
+        }
+        for (let i = 0; i < arrays.length; i++) {
+           let total = arrays[0] * arrays[i];
+            document.getElementById("display").innerHTML = total.toFixed(4);
+        }
+    } 
     else {
         document.getElementById("display").innerText = document.getElementById("display").innerText + num;
     }
